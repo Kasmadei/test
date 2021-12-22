1f0001e1936a35fcf42d83ef9a898e535914333b
Kiryl Koniukh <49252228+Kasmadei@users.noreply.github.com>
2021-12-22 20:15:40 +0100

    added shared code test

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Builder, By, until } = require("selenium-webdriver");
+const { Builder, By, until, Key } = require("selenium-webdriver");
 
 async function authTest() {
   let driver = await new Builder().forBrowser("chrome").build();
@@ -7,15 +7,26 @@ async function authTest() {
     await driver.get("http://localhost:3000/");
     await (await driver.findElement(By.id("log-in-button"))).click();
     // Wait until redirect finishes.
-    await driver.wait(until.urlContains("b2clogin"), 5000);
-
-    // Inputs
+    await driver.manage().setTimeouts({ implicit: 2000 });
+    // // Inputs
     const emailInput = await driver.findElement(By.id("email"));
     emailInput.sendKeys("kasmadeix300@gmail.com");
     const passwordInput = await driver.findElement(By.id("password"));
     passwordInput.sendKeys("kirill-stom98");
     const loginButton = await driver.findElement(By.id("next"));
     await loginButton.click();
+
+    await driver.manage().setTimeouts({ implicit: 2000 });
+
+    // Shared code test:
+    await (await driver.findElement(By.id("Projects"))).click();
+    await (await driver.findElement(By.id("Pet collection"))).click();
+    await (await driver.findElement(By.id("shared-button"))).click();
+
+    const viewLine = await driver.findElement(By.className("view-line"));
+    await viewLine.click();
+    driver.actions().sendKeys("123").perform();
+    await (await driver.findElement(By.id("save-shared-code"))).click();
   } catch (e) {
     console.log(e, "ERROR");
     driver.quit();
